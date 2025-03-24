1b7030d4fa1c7cf0abb6170d5684659f868401cd
DanDan2804 <134505019+DanDan2804@users.noreply.github.com>
2025-03-24 09:04:56 -0300

    Add files via upload

--- exemplo-encontrar.py
+++ exemplo-encontrar.py
@@ -1,7 +1,7 @@
-def exemplo_find(txt: str) -> str:
+def exemplo_encontrar(txt: str) -> str:
     '''Função para encontrar índice inicial da palavra pesquisada'''
 
-    return txt.find('Ciao')
+    return txt.find('Olá')
 
 if __name__ == "__main__":
-    print(exemplo_find("Olá"))
+    print(exemplo_encontrar("Olá"))
--- manipulando-string.py
+++ manipulando-string.py
@@ -1,17 +1,25 @@
-texto = "Nossa aula Manipulando String."
+texto = "Nossa aula manipulando String."
 
 print(texto[0:20:2])
+'''Exibe todas as letras entre 0 a 20'''
 
 print(len(texto))
+'''Indica o tamanho do texto, quantos caracteres ele possui'''
 
 print(texto.count("a"))
+'''Conta quantas vezes o caracter foi usado na String'''
 
 print(texto.count("a", 5, 30))
+'''Conta quantas vezes o caracter foi usado na String entre 5 a 30'''
 
 print(texto.find("aula"))
+'''Procura a palavra pesquisada na string e indica a posição'''
 
 print(texto.find("Python"))
+'''Caso não encontre, dará o resultado -1'''
 
 print('String' in texto)
+'''Confere se a palavra a ser exibida está no texto'''
 
 print('Raphael' in texto)
+'''Caso não esteja, dará o resultado False'''
