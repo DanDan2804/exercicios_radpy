cf99477234d8e88021eb4f466143f08f0e708586
DanDan2804 <134505019+DanDan2804@users.noreply.github.com>
2025-03-24 08:36:22 -0300

    Update manipulando-string.py

--- manipulando-string.py
+++ manipulando-string.py
@@ -7,3 +7,11 @@ print(len(texto))
 print(texto.count("a"))
 
 print(texto.count("a", 5, 30))
+
+print(texto.find("aula"))
+
+print(texto.find("Python"))
+
+print('String' in texto)
+
+print('Raphael' in texto)
